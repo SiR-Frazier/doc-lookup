5759fcac3dbcb0ea46cbb7da8dac7314c84859f0
Bryce Frazier <bx.frazier@gmail.com>
2018-03-23 15:46:40 -0700

    update .get link and start building out results section of main.js

--- src/doctor.js
+++ src/doctor.js
@@ -8,14 +8,22 @@ export class DoctorSearch {
   }
 
   findDoctor(doctor) {
-    let location = this.location
-    const apiKey = process.exports.apiKey
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}&location=${location}`).then(response => {showResults(results)
+    // let location = this.location;
+    // let doctor = this.doctor;
+    const apiKey = process.exports.apiKey;
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?${doctor}&location=${location}&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(response => {showResults(results)
+    }).fail(() => 'Unfortunately, there was an error. Please try again shortly.')
+  }
+
+  showSpecialist(issue) {
+    // let location = this.location;
+    // let issue =  this.issue;
+    const apiKey = process.exports.apiKey;
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(response =>
+    {showResults(results)
     }).fail(() => 'Unfortunately, there was an error. Please try again shortly.')
   }
 
 }
 
-
-// original API with longitude lattitude: `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`
-//
+// API for Portland,OR  ('https://api.betterdoctor.com/2016-03-01/doctors?user_location=37.773%2C-122.413&skip=0&limit=10&user_key=9bdb5b28a2e985c4ad2c1017ac5e8887&location=or-portland')
--- src/main.js
+++ src/main.js
@@ -5,25 +5,32 @@ import { DoctorSearch } from './../doctor.js';
 import './styles.css';
 import '../.env';
 
-const showResults = response => {}
+const showResults = response => {
+  // results:
+  // let firstName = response.data[i].profile.first_name;
+  // let lastName = response.data[i].profile.last_name;
+  // let address = response.data[i].visit_address.street;
+  // let phoneNumber = response.data[i].phones.number;
+  // let newPatients = response.data[i].practices.accepts_new_patients;
+}
 
 $(document).ready(function() {
   let doctorSearch = new DoctorSearch();
 
   $('#search').click(function() {
     event.preventDefault();
-    $('.showDoctors').empty()
+    $('.showDoctors').empty();
     let doctor = $('#doctor').val();
     $('#showDoctors').text(`Here's a list of doctors near you:`)
-    DoctorSearch.prototype.findDoctor(issue,doctor)
+    DoctorSearch.prototype.findDoctor(doctor)
   });
 
   $('#search').click(function() {
     event.preventDefault();
-    $('.showDoctors').empty()
+    $('.showDoctors').empty();
     let issue = $('#issue').val();
-    $('#showDoctors').text(`Here's a list of doctors near you:`)
-    DoctorSearch.prototype.findDoctor(issue,doctor)
+    $('#showDoctors').text(`Here's a list of doctors:`)
+    DoctorSearch.prototype.showSpecialist(issue)
   });
 
 });
