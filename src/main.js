a3e8d03a340bc2f3e0bf158e0ef3b852c7d372ab
Bryce Frazier <bx.frazier@gmail.com>
2018-03-23 10:55:53 -0700

    update form, add logic to js files

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DoctorSearch } from './../js/doctor.js';
-// import './styles.css';
+import './styles.css';
 import '../.env';
 
 const showResults = response => {}
 
 $(document).ready(function() {
+  let doctorSearch = new DoctorSearch();
 
-  event.preventDefault();
-}
+  $('#search').click(function() {
+    event.preventDefault();
+    $('.showDoctors').empty()
+    let doctor = $('#doctor').val();
+
+  });
+
+});
