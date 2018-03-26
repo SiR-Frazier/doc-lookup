fddd3eef8c9ffaeebf8f8042c64a1de1fcf62bc2
sir-frazier <bxfrazier@gmail.com>
2018-03-25 21:42:59 -0700

    update show specialist function

--- src/doctor.js
+++ src/doctor.js
@@ -1,11 +1,14 @@
 import $ from 'jquery';
-// const apiKey = "9bdb5b28a2e985c4ad2c1017ac5e8887"
+const apiKey = "9bdb5b28a2e985c4ad2c1017ac5e8887"
 const parseDoc = (docInfo) => {
   return `
     <li>
       <div>
         <span>${docInfo.profile.first_name}</span>
         <span>${docInfo.profile.last_name}</span>
+        <span>${docInfo.visit_address.street}</span>
+        <span>${docInfo.phones.number}</span>
+        <span>${docInfo.practices.accepts_new_patients}</span>
       </div>
     </li>
   `
@@ -19,23 +22,18 @@ export default class DoctorSearch {
   }
 
 findDoctor(doctor) {
-  debugger
-    const apiKey = process.exports.apiKey;
-
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`)
-      .then(resp => {
-        // const docFirstName = resp.data[0].profile.first_name
-        console.log(resp)
-        resp.data.map(docInfo => $(".showDoctors").append(parseDoc(docInfo)))
+    // const apiKey = process.exports.apiKey;
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctor}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(resp => {
+    resp.data.map(docInfo => $(".showDoctors").append(parseDoc(docInfo)))
       })
       .catch(() => alert('Unfortunately, there was an error. Please try again shortly.'))
   }
 
   showSpecialist(issue) {
     // const apiKey = process.exports.apiKey;
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(response =>
-    {showResults(results)
-    }).fail(() => 'Unfortunately, there was an error. Please try again shortly.')
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(resp => {            resp.data.map(docInfo => $(".showDoctors").append(parseDoc(docInfo)))
+    })
+    .catch(() => alert('Unfortunately, there was an error. Please try again shortly.'))
   }
 
 }
--- src/main.js
+++ src/main.js
@@ -5,16 +5,6 @@ import DoctorSearch from './doctor.js';
 
 import './styles.css';
 
-
-const showResults = response => {
-  // results:
-  // let firstName = response.data[i].profile.first_name;
-  // let lastName = response.data[i].profile.last_name;
-  // let address = response.data[i].visit_address.street;
-  // let phoneNumber = response.data[i].phones.number;
-  // let newPatients = response.data[i].practices.accepts_new_patients;
-}
-
 $(document).ready(function() {
   const doctorSearch = new DoctorSearch();
 
@@ -24,7 +14,7 @@ $(document).ready(function() {
     let doctor = $('#doctor').val();
     $('#showDoctors').text(`Here's a list of doctors near you:`)
     DoctorSearch.prototype.findDoctor(doctor)
-
+  
   });
 
   // $('#search').click(function() {
