e48b06edc04ffc5859e02078c9cfa525fb24381e
Bryce Frazier <bx.frazier@gmail.com>
2018-03-29 13:15:12 -0700

    add note to code in doctor.js

--- src/doctor.js
+++ src/doctor.js
@@ -34,7 +34,7 @@ findDoctor(doctor) {
       })
       .fail(() => alert('Unfortunately, there was an error. Please try again shortly.'))
   }
-
+  //**LOOK TO ADD APPEND instead of push. using hardcode for API for now.
   // showSpecialist(issue) {
   //   let docInfo = [];
   //   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=or-portland&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${apiKey}`).then(resp => {  resp.data.forEach(function(doctor) {
